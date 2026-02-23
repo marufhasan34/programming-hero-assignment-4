5165e8c05bd9e8dea8fd4f5ac06393f692017b9c
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-02-23 23:09:11 +0600

    interview and rejected button toggle added

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let jobsCount = document.getElementById("jobs-count");
 const allFilterBtn = document.getElementById("all-filter-btn");
 const InterviewFilterBtn = document.getElementById("Interview-filter-btn");
 const RejectedFilterBtn = document.getElementById("Rejected-filter-btn");
+const filteredSection = document.getElementById("filtered-section");
 
 const mainContainer = document.querySelector("main");
 
@@ -27,5 +28,140 @@ function toggleStyle(id) {
   RejectedFilterBtn.classList.remove("btn-primary");
 
   const selected = document.getElementById(id);
-  selected.classList.add('btn-primary')
+  selected.classList.add("btn-primary");
+
+  if (id == "Interview-filter-btn") {
+    allCards.classList.add("hidden");
+    filteredSection.classList.remove("hidden");
+  } else if (id == "all-filter-btn") {
+    allCards.classList.remove("hidden");
+    filteredSection.classList.add("hidden");
+  }else if(id == 'Rejected-filter-btn'){
+     allCards.classList.add("hidden");
+     filteredSection.classList.remove("hidden");
+  }
+}
+
+mainContainer.addEventListener("click", function (event) {
+
+  if (event.target.classList.contains("interview-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const jobName = parentNode.querySelector(".job-name").innerText;
+    const jobNeed = parentNode.querySelector(".job-need").innerText;
+    const jobSalary = parentNode.querySelector(".job-salary").innerText;
+    const jobApproval = parentNode.querySelector(".job-approval").innerText;
+    const jobText = parentNode.querySelector(".job-text").innerText;
+    parentNode.querySelector(".job-approval").innerText = "Interview";
+    const cartInfo = {
+      jobName,
+      jobNeed,
+      jobSalary,
+      jobApproval: 'Interview',
+      jobText,
+    };
+    const jobExist = InterviewList.find(
+      (item) => item.jobName == cartInfo.jobName,
+    );
+    if (!jobExist) {
+      InterviewList.push(cartInfo);
+    }
+    calculateCount();
+    renderInterview();
+  }else if (event.target.classList.contains("rejected-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const jobName = parentNode.querySelector(".job-name").innerText;
+    const jobNeed = parentNode.querySelector(".job-need").innerText;
+    const jobSalary = parentNode.querySelector(".job-salary").innerText;
+    const jobApproval = parentNode.querySelector(".job-approval").innerText;
+    const jobText = parentNode.querySelector(".job-text").innerText;
+    parentNode.querySelector(".job-approval").innerText = "rejected";
+    const cartInfo = {
+      jobName,
+      jobNeed,
+      jobSalary,
+      jobApproval: 'rejected',
+      jobText,
+    };
+    const jobExist = RejectedList.find(
+      (item) => item.jobName == cartInfo.jobName,
+    );
+    if (!jobExist) {
+      RejectedList.push(cartInfo);
+    }
+    RejectedList = RejectedList.filter(item => item.jobName != cartInfo.jobName)
+    calculateCount();
+    renderRejected();
+  }
+});
+
+function renderInterview() {
+  filteredSection.innerHTML = "";
+  for (let Interview of InterviewList) {
+    console.log(Interview);
+    let div = document.createElement("div");
+    div.className = "flex justify-between shadow-2xl p-10 rounded-xl";
+    div.innerHTML = `
+      <div class="space-y-5">
+            <h3 class="font-semibold job-name text-xl">${Interview.jobName}</h3>
+            <p class="font-normal job-need text-gray-600">${Interview.jobNeed}</p>
+            <p class="font-normal job-salary text-gray-400">
+              ${Interview.jobSalary}
+            </p>
+            <p class="font-medium btn uppercase job-approval">${Interview.jobApproval}</p>
+            <p class="job-text">
+              ${Interview.jobText}
+            </p>
+            <div class="flex gap-3">
+              <button class="btn btn-outline uppercase btn-success">
+                Interview
+              </button>
+              <button class="btn btn-outline uppercase btn-error">
+                Rejected
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="border rounded-full p-2 border-gray-300">
+              <i class="fa-solid fa-trash-can"></i>
+            </div>
+          </div>
+    `;
+    filteredSection.appendChild(div);
+  }
+}
+
+
+function renderRejected() {
+  filteredSection.innerHTML = "";
+  for (let rejected of RejectedList) {
+    let div = document.createElement("div");
+    div.className = "flex justify-between shadow-2xl p-10 rounded-xl";
+    div.innerHTML = `
+      <div class="space-y-5">
+            <h3 class="font-semibold job-name text-xl">${rejected.jobName}</h3>
+            <p class="font-normal job-need text-gray-600">${rejected.jobNeed}</p>
+            <p class="font-normal job-salary text-gray-400">
+              ${rejected.jobSalary}
+            </p>
+            <p class="font-medium btn uppercase job-approval">${rejected.jobApproval}</p>
+            <p class="job-text">
+              ${rejected.jobText}
+            </p>
+            <div class="flex gap-3">
+              <button class="btn btn-outline uppercase btn-success">
+                Interview
+              </button>
+              <button class="btn btn-outline uppercase btn-error">
+                Rejected
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="border rounded-full p-2 border-gray-300">
+              <i class="fa-solid fa-trash-can"></i>
+            </div>
+          </div>
+    `;
+    filteredSection.appendChild(div);
+  }
 }
