5a0ffa5d0639cf6e40d4ab26383cd75ef1b59250
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-02-23 13:01:01 +0600

    all cards count set

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(1234)
+
+
+let total = document.getElementById("total");
+let Interview = document.getElementById("Interview");
+let Rejected = document.getElementById("Rejected");
+const allCards = document.getElementById("allCards");
+let jobsCount = document.getElementById("jobs-count");
+
+function calculateCount() {
+  total.innerText = allCards.children.length;
+  jobsCount.innerText = allCards.children.length;
+}
+calculateCount();
